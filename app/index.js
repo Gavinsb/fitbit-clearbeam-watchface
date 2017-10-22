c48fe75524765ce91e6445daabdfc42e61093384
Barkowski <barkowski@shinobi-mail.de>
2017-10-22 19:30:44 +0200

    Initiialer Commit

---
+++ app/index.js
@@ -0,0 +1,158 @@
+import clock from "clock";
+import document from "document";
+import userActivity from "user-activity";
+import { HeartRateSensor } from "heart-rate";
+
+import * as util from "../common/utils";
+
+// Update the clock every minute
+clock.granularity = "seconds";
+
+// Get a handle on the <text> element
+let myHour = document.getElementById("myHour");
+let myMinute = document.getElementById("myMinute");
+let mySecond = document.getElementById("mySecond");
+let myDate = document.getElementById("myDate");
+let myDay = document.getElementById("myDay");
+let dailysteps = document.getElementById("mySteps");
+let dailystairs = document.getElementById("myStairs");
+let dailycals = document.getElementById("myCals");
+let dailymins = document.getElementById("myMins");
+let currentheart = document.getElementById("myHR");
+let heartRing = document.getElementById("hrtArc");
+let stepRect = document.getElementById("stepsRect");
+let calRect = document.getElementById("calsRect");
+let stairRect = document.getElementById("stairsRect");
+let minRect = document.getElementById("minsRect");
+let heart = document.getElementById("myHR");
+let otherData = document.getElementById("otherData");
+var weekday = new Array(7);
+weekday[0] = "SONNTAG";
+weekday[1] = "MONTAG";
+weekday[2] = "DIENSTAG";
+weekday[3] = "MITTWOCH";
+weekday[4] = "DONNERSTAG";
+weekday[5] = "FREITAG";
+weekday[6] = "SAMSTAG";
+var monthName = new Array(12);
+monthName[0]  = "JANUAR";
+monthName[1]  = "FEBRUAR";
+monthName[2]  = "MÄRZ";
+monthName[3]  = "APRIL";
+monthName[4]  = "MAI";
+monthName[5]  = "JUNI";
+monthName[6]  = "JULI";
+monthName[7]  = "AUGUST";
+monthName[8]  = "SEPTEMBER";
+monthName[9]  = "OKTOBER";
+monthName[10] = "NOVEMBER";
+monthName[11] = "DEZEMBER";
+
+function updateStats() {
+  // Get Goals to reach and curent values
+  const distanceGoal = userActivity.goals.distance;
+  const metricSteps = "steps";  // distance, calories, elevationGain, activeMinutes
+  const amountSteps = userActivity.today.adjusted[metricSteps] || 0;
+  const stepsGoal = userActivity.goals[metricSteps];
+  const metricCals = "calories";  // distance, calories, elevationGain, activeMinutes
+  const amountCals = userActivity.today.adjusted[metricCals] || 0;
+  const caloriesGoal = userActivity.goals[metricCals];
+  const metricActive = "activeMinutes";
+  const amountActive = userActivity.today.adjusted[metricActive] || 0;
+  const activeGoal = userActivity.goals[metricActive];
+  const metricElevation = "elevationGain";
+  const amountElevation = userActivity.today.adjusted[metricElevation] || 0
+  const elevationGoal = userActivity.goals[metricElevation];
+  let stepString = amountSteps;
+  let calString = amountCals;
+  if ( amountSteps > 999 ) {
+    let dotFill = ".";
+    let hundreds = amountSteps-Math.floor(amountSteps/1000)*1000;
+    if ( hundreds < 10 ) {
+      dotFill =".00";
+    } else if ( hundreds < 100 ) {
+      dotFill = ".0";
+    }
+    stepString = Math.floor(amountSteps/1000) + dotFill + hundreds;
+  }
+  if ( amountCals > 999 ) {
+    let dotFill = ".";
+    let hundreds = amountCals-Math.floor(amountCals/1000)*1000;
+    if ( hundreds < 10 ) {
+      dotFill =".00";
+    } else if ( hundreds < 100 ) {
+      dotFill = ".0";
+    }
+    calString = Math.floor(amountCals/1000) + dotFill + hundreds;
+  }
+  if (caloriesGoal > 5000) {
+    caloriesGoal = 3029;
+  }
+  if (elevationGoal > 80) {
+    elevationGoal = 10;
+  }
+  if (activeGoal > 180) {
+    activeGoal = 30;
+  }
+  dailysteps.innerText = stepString; 
+  let stepWidth = Math.floor(100-100*(amountSteps/stepsGoal));
+  if ( stepWidth < 0 ) {
+    stepWidth = 0;
+  }
+  stepRect.width = stepWidth;
+  dailycals.innerText = calString; 
+  let calWidth = Math.floor(100-100*(amountCals/caloriesGoal));
+  if ( calWidth < 0 ) {
+    calWidth = 0;
+  }
+  calRect.width = calWidth;
+  dailystairs.innerText = amountElevation; 
+  let stairWidth = Math.floor(100-100*(amountElevation/elevationGoal));
+  if ( stairWidth < 0 ) {
+    stairWidth = 0;
+  }
+  stairRect.width = stairWidth;
+  dailymins.innerText = amountActive; 
+  let activeWidth = Math.floor(100-100*(amountActive/activeGoal));
+  if ( activeWidth < 0 ) {
+    activeWidth = 0;
+  }
+  minRect.width = activeWidth;
+}
+
+var hrm = new HeartRateSensor();
+
+hrm.onreading = function () {
+  console.log(hrm.heartRate);
+  currentheart.innerText=hrm.heartRate;
+  let heartAngle = Math.floor(360*(hrm.heartRate/190));
+  if ( heartAngle > 360 ) {
+    heartAngle = 360;
+  }
+  heartRing.sweepAngle = heartAngle;
+}
+hrm.start();
+
+// Update the <text> element with the current time
+function updateClock() {
+  let today = new Date();
+  let day = today.getDate();
+  let wday = today.getDay();
+  let month = today.getMonth()+1;
+  let year = today.getFullYear();
+  let hours = util.monoDigits(util.zeroPad(today.getHours()));
+  let mins = util.monoDigits(util.zeroPad(today.getMinutes()));
+  let secs = util.zeroPad(today.getSeconds());
+  myHour.innerText = `${hours}`;
+  myMinute.innerText = `${mins}`;
+  mySecond.innerText = `${secs}`;
+  myDate.innerText = `${day}. ${monthName[month]}`;
+  myDay.innerText = `${weekday[wday]}`
+  updateStats();
+}
+
+// Update the clock every tick event
+clock.ontick = () => updateClock();
+
+// Don't start with a blank screen
+updateClock();
